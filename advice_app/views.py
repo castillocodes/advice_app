89e2bd1b8f8f8d59b90fdf8c0307469e57d00b91
sergio <janeirosergio@gmail.com>
2021-09-24 11:42:15 -0500

    Stylings

--- advice_app/views.py
+++ advice_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from pip._vendor import requests
+
+url = "https://api.adviceslip.com/advice"
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    data = requests.get(url)
+    json_data = data.json()
+    random_advice = json_data["slip"]
+
+    return render(request, 'index.html', {'random_advice': random_advice})
